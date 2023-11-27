ca011cc3a4daefc37cf4eca96770e09c47588c7f
Klaus Hoehfurtner <HoehfurtnerK190028@sus.htl-grieskirchen.at>
2023-11-27 15:17:51 +0100

    alles bis auf Progressbar-Click Event

--- UserControlRobotik/UserControl1.xaml.cs
+++ UserControlRobotik/UserControl1.xaml.cs
@@ -20,6 +20,14 @@ namespace UserControlRobotik
     /// </summary>
     public partial class UserControl1 : UserControl
     {
+        
+
+        public string? UC_Description
+        {
+            get { return groupBox?.Header?.ToString(); }
+            set { groupBox.Header = value; }
+        }
+
         public UserControl1()
         {
             InitializeComponent();
@@ -27,17 +35,42 @@ namespace UserControlRobotik
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
-
+            slider.Value = 100;
+            backSpdPrgBard.Value = 0;
+            spdPrgBard.Value = 100;
+            pctLabel.Content = "100%";
         }
 
         private void stopBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            slider.Value = 0;
+            spdPrgBard.Value = 0;
+            backSpdPrgBard.Value = 0;
+            pctLabel.Content = "0%";
         }
 
         private void backButton_Click(object sender, RoutedEventArgs e)
         {
+            slider.Value = -100;
+            backSpdPrgBard.Value = 100;
+            spdPrgBard.Value = 0;
+            pctLabel.Content = "-100%";
+        }
 
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(slider.Value > 0)
+            {
+                backSpdPrgBard.Value = 0;
+                spdPrgBard.Value = slider.Value;
+                pctLabel.Content = slider.Value.ToString() + "%";
+            }
+            else
+            {
+                backSpdPrgBard.Value = slider.Value * -1;
+                spdPrgBard.Value = 0;
+                pctLabel.Content = slider.Value.ToString() + "%";
+            }
         }
     }
 }
